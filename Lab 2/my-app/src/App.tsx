ea79e17b08ddddf081b10fef61dbe2b72fbb8eea
Allen Vu <allenvu4838@gmail.com>
2024-10-11 23:32:39 -0700

    Exercise 4.1.1 Done

--- Lab 2/my-app/src/App.tsx
+++ Lab 2/my-app/src/App.tsx
@@ -1,22 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
- return (
-   <div className='app-container'>
-     <div className="note-individual">
-       <button>
-         x
-       </button>
-       <h2> 1st Note Title </h2>
-       <p> 1st Note Content </p>
-       <p> 1st Note Label </p>
-     </div>
-   </div>
-
- );
+  return (
+    <div className="app-container">
+      <div className="note-individual">
+        <button>x</button>
+        <h2> 1st Note Title </h2>
+        <p> 1st Note Content </p>
+        <p> 1st Note Label </p>
+      </div>
+    </div>
+  );
 }
 
-   export default App;
+export default App;
