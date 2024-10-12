56b70c870421033016ecb6841c03fa6dc06e7bd9
Allen Vu <allenvu4838@gmail.com>
2024-10-11 23:58:01 -0700

    Exercise 4.1.2 Done

--- Lab 2/my-app/src/App.tsx
+++ Lab 2/my-app/src/App.tsx
@@ -4,14 +4,27 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="app-container">
-      <div className="note-individual">
-        <button>x</button>
-        <h2> 1st Note Title </h2>
-        <p> 1st Note Content </p>
-        <p> 1st Note Label </p>
+    <form className="note-form">
+      <div>
+        <input placeholder="Note Title"></input>
       </div>
-    </div>
+
+      <div>
+        <textarea></textarea>
+      </div>
+
+      <div>
+        <button type="submit">Create Note</button>
+      </div>
+      <div className="app-container">
+        <div className="note-individual">
+          <button>x</button>
+          <h2> 1st Note Title </h2>
+          <p> 1st Note Content </p>
+          <p> 1st Note Label </p>
+        </div>
+      </div>
+    </form>
   );
 }
 
